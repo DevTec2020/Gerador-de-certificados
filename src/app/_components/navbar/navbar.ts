fb8b9f27fe6b127b91d50c367ca6e469cc5336be
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-08-01 12:38:55 -0300

    feat: craindo navbar com bootstrap

---
+++ src/app/_components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
