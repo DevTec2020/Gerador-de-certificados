ad604598447358ecf8248691285127ebd206d8f0
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-08-06 11:42:06 -0300

    feat: criando pagina de lista de certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecundaryButton } from "./_components/secundary-button/secundary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUI } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, PrimaryButton, SecundaryButton, ItemCertificado, BaseUI, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
