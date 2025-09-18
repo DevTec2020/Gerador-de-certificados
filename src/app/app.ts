671ac19ac80293b5f67de95e159de90a934f5008
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-09-18 20:10:32 -0300

    feat: criando rotas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from "./_components/primary-button/primary-button";
-import { SecundaryButton } from "./_components/secundary-button/secundary-button";
-import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+
+
+
 import { BaseUI } from "./_components/base-ui/base-ui";
-import { Certificados } from "./pages/certificados/certificados";
-import { CertificadoForm } from './pages/certificado-form/certificado-form';
-import { Certificado } from './pages/certificado/certificado';
+
+
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, SecundaryButton, ItemCertificado, BaseUI, Certificados, CertificadoForm, Certificado],
+  imports: [Navbar, BaseUI, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
