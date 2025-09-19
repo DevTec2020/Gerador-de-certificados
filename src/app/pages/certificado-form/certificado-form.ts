d45336cf1632eee8e23b9243733d3b856fd4100d
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-09-19 20:38:31 -0300

    feat: Validando campos do oform

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecundaryButton } from "../../_components/secundary-button/secundary-button";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecundaryButton, PrimaryButton, FormsModule],
+  imports: [SecundaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
@@ -13,4 +14,8 @@ export class CertificadoForm {
   nome: string = "";
   atividade: string = "";
   atividades: string[] = ['Angular', 'React', 'Cobol'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched
+  }
 }
