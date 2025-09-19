fab321277a010f35e79e692ac5e8a095cea1437d
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-09-19 13:39:04 -0300

    feat: Adicionando itens dinamicamete

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecundaryButton } from "../../_components/secundary-button/secundary-button";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecundaryButton, PrimaryButton],
+  imports: [SecundaryButton, PrimaryButton, FormsModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
-export class CertificadoForm {}
+export class CertificadoForm {
+  nome: string = "";
+  atividade: string = "";
+  atividades: string[] = ['Angular', 'React', 'Cobol'];
+}
